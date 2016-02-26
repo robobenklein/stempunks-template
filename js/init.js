ef938df6598b70ea14b6e8ae364202ba7f582d1f
Ben Klein <robobenklein@gmail.com>
2016-02-26 14:53:59 -0500

    Edit init.js for noconflict

--- js/init.js
+++ js/init.js
@@ -1,3 +1,78 @@
-(function($){$(function(){$(window).scroll(function(){var top=$(window).scrollTop();$(".slide-in").each(function(){var thisTop=$(this).offset().top;var height=$(this).height();if((top>(thisTop- height))&&!$(this).hasClass("slid")){$(this).addClass("slid");}});var carouselTop=$('.bg').height();if(top>carouselTop){$('.carousel').carousel('pause');console.log("pause");}
-else{$('.carousel').carousel('cycle');console.log("cycle");}});$('.carousel').carousel({pause:false,interval:8000});$("a[data-toggle='tab']").click(function(){$(this).addClass("active");$(this).parent().siblings().each(function(){$(this).children("a").first().removeClass("active");});});$(window).scroll(function(){if($(window).height()>$(window).scrollTop()){var parallax=($(window).scrollTop()*1)/3;$(".bg img").css('marginTop',parallax+"px");}});$("form .form-control").focus(function(){$(this).siblings("label").first().children("i").first().css({"color":"#aaa","left":0});});$("form .form-control").blur(function(){$(this).siblings("label").first().children("i").first().css({"color":"transparent","left":"-20px"});});$(function(){$('.gallery').mixitup({easing:'snap',  resizeContainer:true});});var $container=$('.masonry-grid');$container.imagesLoaded(function(){$container.masonry({itemSelector:'.masonry-grid .item',gutter:0});$(".masonry-grid .item").css("opacity","1");});$(".collapse").collapse({toggle:false})
-$(".navbar-header a.icon").click(function(){$(this).toggleClass("active");$(this).siblings("a.icon").each(function(){$(this).removeClass("active");var target=$(this).data("target");$(target).collapse("hide");})});$("a[data-toggle='collapse']").click(function(){$(this).parent().parent(".panel-heading").toggleClass("active");});});})(jQuery);
+(function(jQuery) {
+  jQuery(function() {
+    jQuery(window).scroll(function() {
+      var top = jQuery(window).scrollTop();
+      jQuery(".slide-in").each(function() {
+        var thisTop = jQuery(this).offset().top;
+        var height = jQuery(this).height();
+        if ((top > (thisTop - height)) && !jQuery(this).hasClass("slid")) {
+          jQuery(this).addClass("slid");
+        }
+      });
+      var carouselTop = jQuery('.bg').height();
+      if (top > carouselTop) {
+        jQuery('.carousel').carousel('pause');
+        console.log("pause");
+      } else {
+        jQuery('.carousel').carousel('cycle');
+        console.log("cycle");
+      }
+    });
+    jQuery('.carousel').carousel({
+      pause: false,
+      interval: 8000
+    });
+    jQuery("a[data-toggle='tab']").click(function() {
+      jQuery(this).addClass("active");
+      jQuery(this).parent().siblings().each(function() {
+        jQuery(this).children("a").first().removeClass("active");
+      });
+    });
+    jQuery(window).scroll(function() {
+      if (jQuery(window).height() > jQuery(window).scrollTop()) {
+        var parallax = (jQuery(window).scrollTop() * 1) / 3;
+        jQuery(".bg img").css('marginTop', parallax + "px");
+      }
+    });
+    jQuery("form .form-control").focus(function() {
+      jQuery(this).siblings("label").first().children("i").first().css({
+        "color": "#aaa",
+        "left": 0
+      });
+    });
+    jQuery("form .form-control").blur(function() {
+      jQuery(this).siblings("label").first().children("i").first().css({
+        "color": "transparent",
+        "left": "-20px"
+      });
+    });
+    jQuery(function() {
+      jQuery('.gallery').mixitup({
+        easing: 'snap',
+          resizeContainer: true
+      });
+    });
+    var $container = jQuery('.masonry-grid');
+    $container.imagesLoaded(function() {
+      $container.masonry({
+        itemSelector: '.masonry-grid .item',
+        gutter: 0
+      });
+      jQuery(".masonry-grid .item").css("opacity", "1");
+    });
+    jQuery(".collapse").collapse({
+      toggle: false
+    })
+    jQuery(".navbar-header a.icon").click(function() {
+      jQuery(this).toggleClass("active");
+      jQuery(this).siblings("a.icon").each(function() {
+        jQuery(this).removeClass("active");
+        var target = jQuery(this).data("target");
+        jQuery(target).collapse("hide");
+      })
+    });
+    jQuery("a[data-toggle='collapse']").click(function() {
+      jQuery(this).parent().parent(".panel-heading").toggleClass("active");
+    });
+  });
+})(jQuery);
